faaa9860e4ee1e7cb1741fae283ed521c2ba2917
SMU-S4 <dchaveza@uteq.edu.ec>
2024-06-17 11:23:21 -0500

    2do commit

--- app/src/main/java/com/example/semana6/MainActivity.java
+++ app/src/main/java/com/example/semana6/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.semana6;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -30,6 +35,41 @@ public class MainActivity extends AppCompatActivity {
 
 
         AutoCompleteTextView textView = findViewById(R.id.autlistacursos);textView.setAdapter(adapter);
+
+
+
+        String[] listaalumnos = new String[]{ "NOHELIA ADAMARIS CEDEÑO BERMELLO" , "Paco Sanchez" , "Toto Almeria" , "Carlos Cuates" , "Quinto Walter" , "Sixto Parra" , "Soria Hunto" , "Octavio Herrera" , "Novenoso Cedeño" , "Decimoso Quito", "JOSEPH URIEL CABRERA PANTOJA", "WALTER GUSTAVO CADME ZAMBRANO"
+
+
+
+
+
+
+
+        };
+
+        ArrayAdapter<String> adaptador2 =new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listaalumnos);
+
+
+
+        ListView lstlistaalunmos = findViewById(R.id.lvlista);
+
+        lstlistaalunmos.setAdapter(adaptador2);
+        lstlistaalunmos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                               @Override
+                                               public void onItemClick(AdapterView<?> adapterView, View view , int i, long l) {
+                                                   Toast.makeText(getApplicationContext(), "Alumno Seleccionado  " + adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_LONG).show();
+
+                                               }
+                                           }
+
+
+
+        );
+
+
+
+
 
     }
 }
